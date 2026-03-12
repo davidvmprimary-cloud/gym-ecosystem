eb128d48b06a051c90fd60543276b178bd76bf73
David Laptop <davidvm.primary@gmail.com>
2026-03-12 16:20:28 -0600

    feat: configure PWA with offline support and IndexedDB sync

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // your custom config here
+};
+
+export default withPWA(nextConfig);
