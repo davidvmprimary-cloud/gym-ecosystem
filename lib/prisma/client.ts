adef12d387b40881f38a4156a548d80473c2c3e0
David Laptop <davidvm.primary@gmail.com>
2026-03-12 16:46:52 -0600

    fix: migrate to Prisma 7 configuration and setup pg adapter

--- lib/prisma/client.ts
+++ lib/prisma/client.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const connectionString = `${process.env.DATABASE_URL}`
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool as any)
+  return new PrismaClient({ adapter })
 }
 
 declare global {
